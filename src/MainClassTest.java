3b18d438f5f154847d03a4838cfdb616d6c6790a
VolodymyrPysarenko <guarder2009@gmail.com>
2021-03-29 11:01:11 +0300

    add functional getClassNumber

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -5,6 +5,7 @@ public class MainClassTest{
 
     MainClass Main = new MainClass();
 
+
     @Test
     public void checkGetLocalNumber() {
         int numberOne = 14;
@@ -12,5 +13,12 @@ public class MainClassTest{
         Assert.assertTrue("LocalNumber not equal 14", numberOne == numberTwo);
 
     }
+
+    @Test
+    public void testGetClassNumber() {
+        Assert.assertTrue("getClassNumber returns a number less than 45",Main.getClassNumber() >= 45);
+    }
+
+
 
 }
